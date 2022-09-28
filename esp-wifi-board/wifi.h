d6a03a730532c82f6155ede252f5ad17b2737fa3
danny-andersen <danny_andersen@yahoo.com>
2022-09-28 16:52:51 +0100

    removed wifi header

--- esp-wifi-board/wifi.h
+++
@@ -1,9 +0,0 @@
-#define SSID_NAME        "HeyHo"
-#define PASSWORD    "DADADA4566C59EFFEBBEA3690D"
-
-uint8_t serverAddr[4] = {192, 168, 1, 219};
-#define PORT        5000
-
-uint8_t clientIP[4] = {192, 168, 1, 190};
-uint8_t dns[4] = {192, 168, 1, 2};
-uint8_t gateway[4] = {192, 168, 1, 1};
